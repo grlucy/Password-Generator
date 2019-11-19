2831363842fe8fc799c782eb87ce445075d0b0f9
Gina Lucy <53538316+grlucy@users.noreply.github.com>
2019-11-19 15:27:07 -0500

    added comments to main.js

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,3 +1,4 @@
+// Variable declarations
 let allLowers = [
   "a",
   "b",
@@ -74,16 +75,25 @@ let allSpecials = [
 ];
 let passwordLength = document.getElementById("passwordLength");
 
-let confirmLower = document.getElementById("confirmLower");
+let confirmLower = document.getElementById("confirmLower").checked;
 
-let confirmUpper = document.getElementById("confirmUpper");
+let confirmUpper = document.getElementById("confirmUpper").checked;
 
-let confirmNumeric = document.getElementById("confirmNumeric");
+let confirmNumeric = document.getElementById("confirmNumeric").checked;
 
-let confirmSpecial = document.getElementById("confirmSpecial");
+let confirmSpecial = document.getElementById("confirmSpecial").checked;
 
 let userSelections = document.getElementById("userSelections");
 
+// Generate random password using if statements to control array concatenation
+function createPassword() {
+  // come back to this!!!
+  if (confirmLower) {
+    alert("confirmLower is checked");
+  }
+}
+
+// Check whether user entered a number between 8 and 128 for password length; If not, error alert and prevent further action. If they did it correctly, proceed with generating a password.
 function lengthError() {
   console.log(passwordLength.value);
 
@@ -93,9 +103,12 @@ function lengthError() {
     isNaN(passwordLength.value)
   ) {
     alert("Error: Password must have between 8 and 128 characters.");
+  } else {
+    createPassword();
   }
 }
 
+// Add event for user clicking "Generate Password" button
 document
   .getElementById("generatePassword")
   .addEventListener("click", lengthError);
