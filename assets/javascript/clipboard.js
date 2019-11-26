6a25e1ea8674c467c26b69a4c97b95d4e3fcda9b
Gina Lucy <53538316+grlucy@users.noreply.github.com>
2019-11-26 11:34:30 -0500

    disabled copy before password generated

--- assets/javascript/clipboard.js
+++ assets/javascript/clipboard.js
@@ -1,13 +1,17 @@
 // Takes password from #displayBox div, converts it into a temporary text input element, selects it, copies it to clipboard and then deletes the input element
 function copyDisplayBox() {
   var copyText = document.querySelector("#displayBox").innerText;
-  var dummyInput = document.createElement("input");
-  dummyInput.type = "text";
-  dummyInput.value = copyText;
-  document.body.appendChild(dummyInput);
-  dummyInput.select();
-  document.execCommand("copy");
-  document.body.removeChild(dummyInput);
+  if (copyText === "Generate a password!") {
+    return;
+  } else {
+    var dummyInput = document.createElement("input");
+    dummyInput.type = "text";
+    dummyInput.value = copyText;
+    document.body.appendChild(dummyInput);
+    dummyInput.select();
+    document.execCommand("copy");
+    document.body.removeChild(dummyInput);
+  }
 }
 
 // Add event for user clicking "Copy to Clipboard" button
